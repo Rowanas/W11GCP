34f2e70dc65a01d87d52dcf7678378af9f3b96b3
ajpch <ajpchaffey@hotmail.com>
2022-06-22 12:36:10 +0100

    W11GCP-11 opening times ready for review and nav bar add

--- w11gcp/src/Components/OpeningTimes.jsx
+++ w11gcp/src/Components/OpeningTimes.jsx
@@ -3,28 +3,12 @@ import React from 'react';
 const OpeningTimes = () => {
     return (
         <div id="OpeningTimes">
+            <p>
+                Please see below for the Cinema opening times.
+            </p>
+            
             <table>
                 <tbody>
-                    <tr>
-                        <td>Wednesday</td>
-                        <td>8am-9pm</td>
-                    </tr>
-                    <tr>
-                        <td>Thursday</td>
-                        <td>8am-9pm</td>
-                    </tr>
-                    <tr>
-                        <td >Friday</td>
-                        <td>8am-9pm</td>
-                    </tr>
-                    <tr>
-                        <td>Saturday</td>
-                        <td>8am-9pm</td>
-                    </tr>
-                    <tr>
-                        <td>Sunday</td>
-                        <td>8am-9pm</td>
-                    </tr>
                     <tr>
                         <td>Monday</td>
                         <td>8am-9pm</td>
@@ -32,6 +16,26 @@ const OpeningTimes = () => {
                     <tr>
                         <td>Tuesday</td>
                         <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td >Wednesday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Thursday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Friday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Saturday</td>
+                        <td>8am-11pm</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday</td>
+                        <td>10am-7pm</td>
                     </tr>
                 </tbody>
             </table>
