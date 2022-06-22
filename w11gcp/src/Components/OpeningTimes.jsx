a760a8e89987aee0af7b243328a0861a7c79548d
ajpch <ajpchaffey@hotmail.com>
2022-06-22 10:42:09 +0100

    W11GCP-11, opening times updated

--- w11gcp/src/Components/OpeningTimes.jsx
+++ w11gcp/src/Components/OpeningTimes.jsx
@@ -1,7 +1,42 @@
-
+import React from 'react';
 
 const OpeningTimes = () => {
-    return (  );
+    return (
+        <div id="OpeningTimes">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Wednesday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Thursday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td >Friday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Saturday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Monday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                    <tr>
+                        <td>Tuesday</td>
+                        <td>8am-9pm</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
- 
-export default OpeningTimes ;
+
+export default OpeningTimes;
