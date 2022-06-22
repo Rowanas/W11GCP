8ea758e399f99aa170196ab209556bda0905c006
ajpch <ajpchaffey@hotmail.com>
2022-06-22 13:35:59 +0100

    W11GCP- 14 and 15

--- w11gcp/src/App.js
+++ w11gcp/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import ClassificationPage from './Components/ClassificationPage';
 import OpeningTimes from './Components/OpeningTimes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       < OpeningTimes />
-        
-      </header>
+
+      < OpeningTimes />
+      < ClassificationPage />
+
     </div>
   );
 }
