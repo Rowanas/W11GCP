883d272787800835b558806484d588d6842349fb
Matt <mglion82@yahoo.co.uk>
2022-06-24 14:29:14 +0100

    About page with connection glitches

--- w11gcp/src/Components/AboutPage.jsx
+++ w11gcp/src/Components/AboutPage.jsx
@@ -1,64 +1,100 @@
 import React from 'react'
+import { Container, Row, Col } from "reactstrap";
 import { useNavigate } from "react-router-dom";
+import NavigationBar from "./NavigationBar";
 
 const AboutPage = () => {
     let navigate = useNavigate();
+    
     return (
         <>
-            <div className="Alex">
-                <h2>Who are you?</h2>
-                <p>Hi, My name is Alex Chaffey.</p>
-                <h2>Abit about you</h2>
-                <p>Im a new developer and have alongside my team created this site</p>
-                <h2>Twitter</h2>
-                <p>@MrBingbong</p>
-                <h2>Email address</h2>
-                <p>MrBingBong@gmail.co.uk</p>
-            </div>
+        <NavigationBar/>
+        <p/>
+            <Container>
+            <Row>
+                <Col><h2>Who are you?</h2></Col>
+                <Col><p>Hi, My name is Alex Chaffey.</p></Col>
+                <Col><h2>Abit about you</h2></Col>
+                <Col><p>Im a new developer and have alongside my team created this site</p></Col>
+                <Col><h2>Twitter</h2></Col>
+                <Col><p>@MrBingbong</p></Col>
+                <Col><h2>Email address</h2></Col>
+                <Col><p>MrBingBong@gmail.co.uk</p></Col>
+                </Row>
+                
+                
+                
+                
+                
+                
+                
+                
+            
 
-            <div className="Jehad">
-                <h2>Who are you?</h2>
-                <p>Hello, My name is Jehad AbdelBaqi.</p>
-                <h2>Abit about you</h2>
-                <p>Im a new developer with experience in Python, and I have alongside my team created this site</p>
-                <h2>Twitter</h2>
-                <p>@SupremeAbdelBaqi</p>
-                <h2>Email address</h2>
-                <p>slabdelBaqi@gmail.co.uk</p>
-            </div>
+            <Row>
+                <Col><h2>Who are you?</h2></Col>
+                <Col><p>Hello, My name is Jehad AbdelBaqi.</p></Col>
+                <Col><h2>Abit about you</h2></Col>
+                <Col><p>Im a new developer with experience in Python, and I have alongside my team created this site</p></Col>
+                <Col><h2>Twitter</h2></Col>
+                <Col><p>@SupremeAbdelBaqi</p></Col>
+                <Col><h2>Email address</h2></Col>
+                <Col><p>slabdelBaqi@gmail.co.uk</p></Col>
+                </Row>
 
-            <div className="Rowan">
-                <h2>Who are you?</h2>
-                <p>Hi, My name is Rowan Wittaker.</p>
-                <h2>Abit about you</h2>
-                <p>Im a new developer and have alongside my team created this site</p>
-                <h2>Twitter</h2>
-                <p>@Roleplayer</p>
-                <h2>Email address</h2>
-                <p>rpreaper@yahoo.co.uk</p>
-            </div>
+                
+                
+                
+                
+                
+                
+                
+                
+            
 
-            <div className="Wojciech">
-                <h2>Who are you?</h2>
-                <p>Hi, My name is Wojciech.</p>
-                <h2>Abit about you</h2>
-                <p>Im a new developer but have alot of experience with technology</p>
-                <h2>Twitter</h2>
-                <p>@Anonyouwish</p>
-                <h2>Email address</h2>
-                <p>Wojciechmate@hotmail.com</p>
-            </div>
+            <Row>
+                <Col><h2>Who are you?</h2></Col>
+                <Col><p>Hi, My name is Rowan Wittaker.</p></Col>
+                <Col><h2>Abit about you</h2></Col>
+                <Col><p>Im a new developer and have alongside my team created this site</p></Col>
+                <Col><h2>Twitter</h2></Col>
+                <Col><p>@Roleplayer</p></Col>
+                <Col><h2>Email address</h2></Col>
+                <Col><p>rpreaper@yahoo.co.uk</p></Col>
+                </Row>
 
-            <div className="Matt">
-                <h2>Who are you?</h2>
-                <p>Hi, My name is Matt Grimsey.</p>
-                <h2>Abit about you</h2>
-                <p>Ive had experience in the past with frontend work years ago and have refreshed my skills which has allowed me to alongside my team create this site</p>
-                <h2>Twitter</h2>
-                <p>@MattyG</p>
-                <h2>Email address</h2>
-                <p>Mattg@gmail.co.uk</p>
-            </div>
+
+            <Row>
+                <Col><h2>Who are you?</h2></Col>
+                <Col><p>Hi, My name is Wojciech.</p></Col>
+                <Col><h2>Abit about you</h2></Col>
+                <Col><p>Im a new developer but have alot of experience with technology</p></Col>
+                <Col><h2>Twitter</h2></Col>
+                <Col><p>@Anonyouwish</p></Col>
+                <Col><h2>Email address</h2></Col>
+                <Col><p>Wojciechmate@hotmail.com</p></Col>
+                </Row>
+                
+                
+                
+                
+                
+                
+                
+                
+            
+
+            <Row>
+                <Col><h2>Who are you?</h2></Col>
+                <Col><p>Hi, My name is Matt Grimsey.</p></Col>
+                <Col><h2>Abit about you</h2></Col>
+                <Col><p>Ive had experience in the past with frontend work years ago and have refreshed my skills which has allowed me to alongside my team create this site</p></Col>
+                <Col><h2>Twitter</h2></Col>
+                <Col><p>@MattyG</p></Col>
+                <Col><h2>Email address</h2></Col>
+                <Col><p>Mattg@gmail.co.uk</p></Col>
+                </Row>
+            </Container>
 
             <button onClick={() => {
                 navigate("/ClassificationPage.jsx");
