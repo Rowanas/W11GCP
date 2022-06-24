302302ab63a1d34a7b99fdbaea486a176f1cf3ca
voytus <voytus@protonmail.com>
2022-06-24 10:04:55 +0100

    W11GCP-21 for test

--- w11gcp/src/App.js
+++ w11gcp/src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {loadStripe} from '@stripe/stripe-js';
-import {
-  CardElement,
-  Elements,
-  useStripe,
-  useElements,
-} from '@stripe/react-stripe-js';
+import React from "react";
+
+import "./styles.css";
+
+import { Elements } from "@stripe/react-stripe-js";
+
+import { loadStripe } from "@stripe/stripe-js";
+
+import CheckoutForm from "./CheckoutForm";
+
+
+const stripePromise = loadStripe("pk_test_pk_test_51LDVSiLFassR4rxhu3vH8C8DTn71kfXW66MmAESDafqx5R8jwjlJsHJ4zIJ94pdq7lXCr2DeRdwUCWCkzfmXuAbv00l6orsNL7");
+
+
+const App = () => {
+
+  return (
+
+    <div className="App">
+
+      <div className="product">
+
+        <img
+
+          src="https://img.xcitefun.net/users/2014/07/357279,xcitefun-singham-returns-poster-9.jpg"
+
+          alt="movie"
+
+          style={{ width: "100%", height: "auto" }}
+
+        />
+
+        <div>
+
+          <Elements stripe={stripePromise}>
+
+            <CheckoutForm />
+
+          </Elements>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  );
+
+};
+
+
+export default App;
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { loadStripe } from '@stripe/stripe-js';
+// import {
+//   CardElement,
+//   Elements,
+//   useStripe,
+//   useElements,
+// } from '@stripe/react-stripe-js';
 
 
 
-const stripePromise = loadStripe('pk_test_pk_test_51LDVSiLFassR4rxhu3vH8C8DTn71kfXW66MmAESDafqx5R8jwjlJsHJ4zIJ94pdq7lXCr2DeRdwUCWCkzfmXuAbv00l6orsNL7');
+// const stripePromise = loadStripe('pk_test_pk_test_51LDVSiLFassR4rxhu3vH8C8DTn71kfXW66MmAESDafqx5R8jwjlJsHJ4zIJ94pdq7lXCr2DeRdwUCWCkzfmXuAbv00l6orsNL7');
 
-const App = () => (
-  <Elements stripe={stripePromise}>
-    <CheckoutForm />
-  </Elements>
-);
+// const App = () => (
+//   <Elements stripe={stripePromise}>
+//     <CheckoutForm />
+//   </Elements>
+// );
 
-ReactDOM.render(<App />, document.body);
+// ReactDOM.render(<App />, document.body);
 
 // import React, { useState } from "react";
 // import {
--- w11gcp/src/index.js
+++ w11gcp/src/index.js
@@ -1,22 +1,44 @@
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(
+import React from "react";
 
-'pk_test_pk_test_51LDVSiLFassR4rxhu3vH8C8DTn71kfXW66MmAESDafqx5R8jwjlJsHJ4zIJ94pdq7lXCr2DeRdwUCWCkzfmXuAbv00l6orsNL7'
+import ReactDOM from "react-dom";
+
+
+import App from "./App";
+
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+
+  <React.StrictMode>
+
+    <App />
+
+  </React.StrictMode>,
+
+  rootElement
+
 );
+// import {Elements} from '@stripe/react-stripe-js';
+// import {loadStripe} from '@stripe/stripe-js';
 
-function App() {
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret: '{{CLIENT_SECRET}}',
-  };
+// // Make sure to call `loadStripe` outside of a component’s render to avoid
+// // recreating the `Stripe` object on every render.
+// const stripePromise = loadStripe(
 
-  return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
-    </Elements>
-  );
-};
+// 'pk_test_pk_test_51LDVSiLFassR4rxhu3vH8C8DTn71kfXW66MmAESDafqx5R8jwjlJsHJ4zIJ94pdq7lXCr2DeRdwUCWCkzfmXuAbv00l6orsNL7'
+// );
+
+// function App() {
+//   const options = {
+//     // passing the client secret obtained from the server
+//     clientSecret: '{{CLIENT_SECRET}}',
+//   };
+
+//   return (
+//     <Elements stripe={stripePromise} options={options}>
+//       <CheckoutForm />
+//     </Elements>
+//   );
+// };
