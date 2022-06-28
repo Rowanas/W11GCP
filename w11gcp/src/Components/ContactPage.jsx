b80954623737b5d3cdb7892adc07d1798267aa23
MGEX82 <103578442+MGEX82@users.noreply.github.com>
2022-06-28 12:05:14 +0100

    Bit of styling

--- w11gcp/src/Components/ContactPage.jsx
+++ w11gcp/src/Components/ContactPage.jsx
@@ -11,13 +11,13 @@ const ContactPage = () => {
             <Container>
 
             <Col>
-            <Row><h1>Contact form</h1></Row>
+            <Row><h1><b>Contact form</b></h1></Row>
                 
             <Row><input type="text" placeholder="Your Name" /></Row>
             <Row><input type="email" placeholder="Your email address" /></Row>
             <Row><textarea placeholder="Your message"></textarea></Row>
-            <Row><button>Send Message</button></Row>
-            <Row><span>Thank you for your message, we will be in touch in no time!</span>
+            <Row><button><h4>Send Message</h4></button></Row>
+            <Row><span><h2>Thank you for your message, we will be in touch in no time!</h2></span>
         </Row>
                 <Row><h2>Postal address: QA Cinemas, Bartholemew Street, Exeter, EX4 1RD</h2></Row>
               
