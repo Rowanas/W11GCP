29945d325c1dc2bfbbc245f62a710bcf0af3da2e
Matt <mglion82@yahoo.co.uk>
2022-06-28 09:33:20 +0100

    About page finished(still require film list)

--- w11gcp/src/Components/AboutPage.jsx
+++ w11gcp/src/Components/AboutPage.jsx
@@ -8,92 +8,63 @@ const AboutPage = () => {
     
     return (
         <>
-        <NavigationBar/>
+       
         <p/>
             <Container>
-            <Row>
-                <Col><h2>Who are you?</h2></Col>
-                <Col><p>Hi, My name is Alex Chaffey.</p></Col>
-                <Col><h2>Abit about you</h2></Col>
-                <Col><p>Im a new developer and have alongside my team created this site</p></Col>
-                <Col><h2>Twitter</h2></Col>
-                <Col><p>@MrBingbong</p></Col>
-                <Col><h2>Email address</h2></Col>
-                <Col><p>MrBingBong@gmail.co.uk</p></Col>
-                </Row>
+            <Col>
+                <Row><h2>Who are you?</h2></Row>
+                <Row><p>Hi, My name is Alex Chaffey.</p></Row>
+                <Row><h2>Abit about you</h2></Row>
+                <Row><p>Im a new developer and have alongside my team created this site</p></Row>
+                <Row><h2>Twitter</h2></Row>
+                <Row><p>@MrBingbong</p></Row>
+                <Row><h2>Email address</h2></Row>
+                <Row><p>MrBingBong@gmail.co.uk</p></Row>
+                </Col>
                 
-                
-                
-                
-                
-                
-                
-                
-            
+                <Col>
+                <Row><h2>Who are you?</h2></Row>
+                <Row><p>Hello, My name is Jehad AbdelBaqi.</p></Row>
+                <Row><h2>Abit about you</h2></Row>
+                <Row><p>Im a new developer with experience in Python, and I have alongside my team created this site</p></Row>
+                <Row><h2>Twitter</h2></Row>
+                <Row><p>@SupremeAbdelBaqi</p></Row>
+                <Row><h2>Email address</h2></Row>
+                <Row><p>slabdelBaqi@gmail.co.uk</p></Row>
+                </Col>
 
-            <Row>
-                <Col><h2>Who are you?</h2></Col>
-                <Col><p>Hello, My name is Jehad AbdelBaqi.</p></Col>
-                <Col><h2>Abit about you</h2></Col>
-                <Col><p>Im a new developer with experience in Python, and I have alongside my team created this site</p></Col>
-                <Col><h2>Twitter</h2></Col>
-                <Col><p>@SupremeAbdelBaqi</p></Col>
-                <Col><h2>Email address</h2></Col>
-                <Col><p>slabdelBaqi@gmail.co.uk</p></Col>
-                </Row>
+                <Col>
+                <Row><h2>Who are you?</h2></Row>
+                <Row><p>Hi, My name is Rowan Wittaker.</p></Row>
+                <Row><h2>Abit about you</h2></Row>
+                <Row><p>Im a new developer and have alongside my team created this site</p></Row>
+                <Row><h2>Twitter</h2></Row>
+                <Row><p>@Roleplayer</p></Row>
+                <Row><h2>Email address</h2></Row>
+                <Row><p>rpreaper@yahoo.co.uk</p></Row>
+                </Col>
 
+                <Col>
+                <Row><h2>Who are you?</h2></Row>
+                <Row><p>Hi, My name is Wojciech.</p></Row>
+                <Row><h2>Abit about you</h2></Row>
+                <Row><p>Im a new developer but have alot of experience with technology</p></Row>
+                <Row><h2>Twitter</h2></Row>
+                <Row><p>@Anonyouwish</p></Row>
+                <Row><h2>Email address</h2></Row>
+                <Row><p>Wojciechmate@hotmail.com</p></Row>
+                </Col>
                 
-                
-                
-                
-                
-                
-                
-                
-            
-
-            <Row>
-                <Col><h2>Who are you?</h2></Col>
-                <Col><p>Hi, My name is Rowan Wittaker.</p></Col>
-                <Col><h2>Abit about you</h2></Col>
-                <Col><p>Im a new developer and have alongside my team created this site</p></Col>
-                <Col><h2>Twitter</h2></Col>
-                <Col><p>@Roleplayer</p></Col>
-                <Col><h2>Email address</h2></Col>
-                <Col><p>rpreaper@yahoo.co.uk</p></Col>
-                </Row>
-
-
-            <Row>
-                <Col><h2>Who are you?</h2></Col>
-                <Col><p>Hi, My name is Wojciech.</p></Col>
-                <Col><h2>Abit about you</h2></Col>
-                <Col><p>Im a new developer but have alot of experience with technology</p></Col>
-                <Col><h2>Twitter</h2></Col>
-                <Col><p>@Anonyouwish</p></Col>
-                <Col><h2>Email address</h2></Col>
-                <Col><p>Wojciechmate@hotmail.com</p></Col>
-                </Row>
-                
-                
-                
-                
-                
-                
-                
-                
-            
-
-            <Row>
-                <Col><h2>Who are you?</h2></Col>
-                <Col><p>Hi, My name is Matt Grimsey.</p></Col>
-                <Col><h2>Abit about you</h2></Col>
-                <Col><p>Ive had experience in the past with frontend work years ago and have refreshed my skills which has allowed me to alongside my team create this site</p></Col>
-                <Col><h2>Twitter</h2></Col>
-                <Col><p>@MattyG</p></Col>
-                <Col><h2>Email address</h2></Col>
-                <Col><p>Mattg@gmail.co.uk</p></Col>
-                </Row>
+                <Col>
+                <Row><h2>Who are you?</h2></Row>
+                <Row><p>Hi, My name is Matt Grimsey.</p></Row>
+                <Row><h2>Abit about you</h2></Row>
+                <Row><p>Ive had experience in the past with frontend work years ago and have refreshed my skills which has allowed me to alongside my team create this site</p></Row>
+                <Row><h2>Twitter</h2></Row>
+                <Row><p>@MattyG</p></Row>
+                <Row><h2>Email address</h2></Row>
+                <Row><p>Mattg@gmail.co.uk</p></Row>
+                </Col>
             </Container>
 
             <button onClick={() => {
