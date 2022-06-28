9c38c526e6187a9cd83f2ccc84d9b329b67dfabe
MGEX82 <103578442+MGEX82@users.noreply.github.com>
2022-06-28 11:53:19 +0100

    MG adding contact page

--- w11gcp/src/Components/ContactPage.jsx
+++ w11gcp/src/Components/ContactPage.jsx
@@ -1,6 +1,30 @@
+import React from 'react'
+import { Container, Row, Col } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+
 const ContactPage = () => {
+    let navigate = useNavigate();
     return ( 
-        <h1>Contacts</h1>
+        <>
+       
+        <p/>
+            <Container>
+
+            <Col>
+            <Row><h1>Contact form</h1></Row>
+                
+            <Row><input type="text" placeholder="Your Name" /></Row>
+            <Row><input type="email" placeholder="Your email address" /></Row>
+            <Row><textarea placeholder="Your message"></textarea></Row>
+            <Row><button>Send Message</button></Row>
+            <Row><span>Thank you for your message, we will be in touch in no time!</span>
+        </Row>
+                <Row><h2>Postal address: QA Cinemas, Bartholemew Street, Exeter, EX4 1RD</h2></Row>
+              
+                </Col>
+                </Container>
+               
+        </>
      );
 }
  
