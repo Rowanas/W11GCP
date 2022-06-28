3c89b0bd746be095ebe3ec7a147ae5853e52684a
ajpch <ajpchaffey@hotmail.com>
2022-06-28 15:17:35 +0100

    W11GCP-16 Screens component completed-AC

--- w11gcp/src/App.js
+++ w11gcp/src/App.js
@@ -3,29 +3,69 @@ import './App.css';
 import HomePage from "./Components/HomePage";
 import AboutPage from './Components/AboutPage';
 import ClassificationPage from './Components/ClassificationPage';
-import OpeningTimes from './Components/OpeningTimes';
 import NavigationBar from './Components/NavigationBar'
 import Footer from './Components/Footer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import GettingHerePage from './Components/GettingHerePage';
-import FilmList from "./Components/FilmsList";
 import BookingsPage from "./Components/BookingPage";
 import Screens from "./Components/Screens";
 import Partners from "./Components/Partners";
 import Comment from "./Components/Comment";
 import ContactPage from "./Components/ContactPage";
+import films from "./Components/films.JSON"
+import Card from './Components/Card';
+import SearchBar from "./Components/SearchBar";
+import { useState } from 'react';
+import * as Fuse from './Components/fuse.js';
 
 
+  // const [data, setData] = useState(films);
 
-function App() {
+  // const searchData = (pattern) => {
+  //   if (!pattern) {
+  //     setData(films);
+  //     return;
+  //   }
+
+  //   const fuse = new Fuse(data, {
+  //     keys: ["title", "author"],
+  //   });
+
+  //   const result = fuse.search(pattern);
+  //   const matches = [];
+  //   if (!result.length) {
+  //     setData([]);
+  //   } else {
+  //     result.forEach(({item}) => {
+  //       matches.push(item);
+  //     });
+  //     setData(matches);
+  //   }
+  // };  
+//     <div>
+//       <SearchBar
+//         placeholder="Search Term"
+//         onChange={(e) => console.log(e.target.value)}
+//        />
+//       <div className="Container">
+//         {films.map((item) => (
+//           <Card {...item} key={item.name} />
+//         ))}
+//       </div>
+//     </div>
+    
+//   );
+// }
+function App() { 
   return (
+
     <div className="page-container">
       <div className="content-wrap">
       <Router>
+        <SearchBar/>
         <NavigationBar/>
           <Routes>
             <Route path="/HomePage.jsx" element={<HomePage/>}/>
-            <Route path="/FilmsList.jsx" element={<FilmList/>}/>
             <Route path="/BookingPage.jsx" element={<BookingsPage/>}/>
             <Route path="/Screens.jsx" element={<Screens/>}/>
             <Route path="/GettingHerePage.jsx" element={<GettingHerePage/>}/>
@@ -34,6 +74,7 @@ function App() {
             <Route path="/AboutPage.jsx" element={<AboutPage/>}/>
             <Route path="/Partners.jsx" element={<Partners/>}/>
             <Route path="/ContactPage.jsx" element={<ContactPage/>}/>
+          
           </Routes>
         <Footer/>
       </Router>
