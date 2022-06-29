28388e2de1b89c18a375705ea1c44529b8f320b0
MGEX82 <103578442+MGEX82@users.noreply.github.com>
2022-06-29 12:14:10 +0100

    MG added validation to contact page

--- w11gcp/src/Components/ContactPage.jsx
+++ w11gcp/src/Components/ContactPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Container, Row, Col } from "reactstrap";
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { submit} from 'react';
+
 
 const ContactPage = () => {
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [message, setMessage] = useState('');
+const [emailSent, setEmailSent] = useState(false);
     let navigate = useNavigate();
     return ( 
         <>
@@ -13,12 +20,11 @@ const ContactPage = () => {
             <Col>
             <Row><h1><b>Contact form</b></h1></Row>
                 
-            <Row><input type="text" placeholder="Your Name" /></Row>
-            <Row><input type="email" placeholder="Your email address" /></Row>
-            <Row><textarea placeholder="Your message"></textarea></Row>
-            <Row><button><h4>Send Message</h4></button></Row>
-            <Row><span><h2>Thank you for your message, we will be in touch in no time!</h2></span>
-        </Row>
+            <Row><input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} /></Row>
+            <Row><input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} /></Row>
+            <Row><textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea></Row>
+            <Row><button onClick={submit}>Send Message</button></Row>
+            <Row> <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span></Row>
                 <Row><h2>Postal address: QA Cinemas, Bartholemew Street, Exeter, EX4 1RD</h2></Row>
               
                 </Col>
